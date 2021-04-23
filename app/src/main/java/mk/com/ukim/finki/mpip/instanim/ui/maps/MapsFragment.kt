304e22be82370b0221f2baeb50cdb54a3da39b6d
sjovanov <stefan.jovanov98@gmail.com>
2021-04-23 20:57:43 +0200

    Feat: Double click navigates to details

--- app/src/main/java/mk/com/ukim/finki/mpip/instanim/ui/maps/MapsFragment.kt
+++ app/src/main/java/mk/com/ukim/finki/mpip/instanim/ui/maps/MapsFragment.kt
@@ -1,16 +1,18 @@
 package mk.com.ukim.finki.mpip.instanim.ui.maps
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import mk.com.ukim.finki.mpip.instanim.R
 import mk.com.ukim.finki.mpip.instanim.data.entity.Post
@@ -25,6 +27,8 @@ class MapsFragment : Fragment() {
     private val binding
         get() = _binding!!
     private var posts: List<Post>? = null
+    private val mapsFragmentMapHandler: MapsFragmentMapHandler = MapsFragmentMapHandler()
+    private var markerMap: HashMap<Marker, String> = HashMap()
     private val callback = OnMapReadyCallback { googleMap ->
         /**
          * Manipulates the map once available.
@@ -35,14 +39,18 @@ class MapsFragment : Fragment() {
          * install it inside the SupportMapFragment. This method will only be triggered once the
          * user has installed Google Play services and returned to the app.
          */
-        var lastPost:LatLng? = null
+        var lastPost: LatLng? = null
         posts?.forEach { post ->
             run {
                 val latLng: LatLng? = post.lat?.let { post.lng?.let { it1 -> LatLng(it, it1) } }
-                googleMap.addMarker(latLng?.let { MarkerOptions().position(it).title(post.description?.take(15)) })
+                val tmpMarker = googleMap.addMarker(latLng?.let {
+                    MarkerOptions().position(it).title(post.description?.take(15))
+                })
+                markerMap[tmpMarker] = post.postId
                 lastPost = latLng
             }
         }
+        googleMap.setOnMarkerClickListener(mapsFragmentMapHandler)
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(lastPost))
     }
 
@@ -84,8 +92,29 @@ class MapsFragment : Fragment() {
 
     }
 
+    fun navigateToDetails(postId: String) {
+        val action = MapsFragmentDirections.actionMapsFragmentToPostDetailsFragment(postId)
+        findNavController().navigate(action)
+    }
+
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    inner class MapsFragmentMapHandler : GoogleMap.OnMarkerClickListener {
+
+        var clickedMarker: Marker? = null
+
+        override fun onMarkerClick(marker: Marker?): Boolean {
+            if (clickedMarker != marker) {
+                clickedMarker = marker
+            } else {
+                clickedMarker = null
+                navigateToDetails(markerMap[marker].toString())
+            }
+
+            return false
+        }
     }
 }
